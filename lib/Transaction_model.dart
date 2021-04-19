18d6714a90f30acdf51dc844f0e86c7d0359fa8a
kaushikpuka1998 <kaushikghosh199832@gmail.com>
2021-04-20 03:31:17 +0530

    Some Data added in transaction Part to check

--- lib/Transaction_model.dart
+++ lib/Transaction_model.dart
@@ -13,18 +13,12 @@ List<Transaction>transactiond = transactiondata.map((e) => Transaction(
 ),
 ).toList();
 var transactiondata = [
-  {
-    "name": "Wow Momo,Kolkata",
-    "date": "11th Dec,2020",
-    "image" : "images/Wow_momo.jpg",
-    "amount" : "-120.25"
 
-  },
   {
-    "name": "Flipkart Online",
-    "date": "05th Feb,2021",
-    "image" : "images/flipkart.png",
-    "amount": "-748,23"
+    "name": "Amazon Purchase",
+    "date": "19th March,2021",
+    "image" : "images/amazon.png",
+    "amount": "-4458.00"
 
   },
   {
@@ -39,6 +33,34 @@ var transactiondata = [
     "date": "12th March,2021",
     "image" : "images/coolwings.png",
     "amount": "-1489.45"
+
+  },
+  {
+    "name": "Flipkart Online",
+    "date": "05th Feb,2021",
+    "image" : "images/flipkart.png",
+    "amount": "-748,23"
+
+  },
+  {
+    "name": "Wow Momo,Kolkata",
+    "date": "11th Dec,2020",
+    "image" : "images/Wow_momo.jpg",
+    "amount" : "-120.25"
+
+  },
+  {
+    "name": "Ola Cab",
+    "date": "12th Nov,2020",
+    "image" : "images/ola.jpg",
+    "amount": "-330.00"
+
+  },
+  {
+    "name": "Grofers Reversal",
+    "date": "10th Oct,2020",
+    "image" : "images/groffers.jpg",
+    "amount": "+330.00"
 
   },
 
